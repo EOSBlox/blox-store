5ca2d5bbc7d0d27d79b6b5fa670041a36872560b
Will in China <email.will.in.china@gmail.com>
2018-08-30 21:19:43 +0800

    version 1

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # \<blox-store\>
 
-get and set JSON data from local storage
+Get, set and delete JSON data from local storage
 
 ## Install the Polymer-CLI
 
 First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) and npm (packaged with [Node.js](https://nodejs.org)) installed. Run `npm install` to install your element's dependencies, then run `polymer serve` to serve your element locally.
+
+## Install blox-store
+
+```
+$ npm install blox-store
+```
 
 ## Viewing Your Element
 
@@ -18,4 +24,33 @@ $ polymer serve
 $ polymer test
 ```
 
-Your application is already set up to be tested via [web-component-tester](https://github.com/Polymer/web-component-tester). Run `polymer test` to run your application's test suite locally.
+## Import
+
+```
+$ import 'blox-store';
+```
+
+## Basic Use
+
+```html
+<blox-store
+    key="myApp"
+    value="{'name': 'Will'}"
+    operation="set">
+</blox-store>
+```
+
+## Javascript Use
+
+```html
+<blox-store id="bloxStore"></blox-store>
+<script>
+    this.$.bloxStore.set('myApp', {'name': 'Will'})
+    .then((hash) => {
+        // Do Something
+    })
+    .catch((err) => {
+        // Do Something
+    })
+</script>
+```
--- blox-store.js
+++ blox-store.js
@@ -16,17 +16,78 @@ class BloxStore extends PolymerElement {
           display: block;
         }
       </style>
-      <h2>Hello [[prop1]]!</h2>
+      <template is="dom-if" if="{{debug}}">
+        <p>[[key]]</p>
+        <p>[[value]]</p>
+        <p>[[operation]]</p>
+        <p>[[result]]</p>
+      </template>
     `;
   }
   static get properties() {
     return {
-      prop1: {
+      key: {
         type: String,
-        value: 'blox-store',
+      },
+      value: {
+        type: String,
+      },
+      debug: {
+        type: Boolean,
+        value: false,
+      },
+      operation: {
+        type: String,
+        observer: "_start"
+      },
+      result: {
+        type: String,
+        notify: true,
+        reflectToAttribute: true,
+      },
+      overwrite: {
+        type: Boolean,
+        value: false,
+      },
+      error: {
+        type: String,
+        notify: true,
+        reflectToAttribute: true,
       },
     };
   }
-}
 
-window.customElements.define('blox-store', BloxStore);
+  _start(){
+    if(this.operation == 'set' && this.key && this.value){
+      this.set(this.key, this.value)
+    } else if (this.operation == 'get' && this.key){
+      this.get(this.key)
+    } else if (this.operation == 'delete' && this.key){
+      this.delete(this.key)
+    } else {
+      this.error = "wrong arguments"
+    }
+  }
+
+  set(key, value){
+    return new Promise((resolve, reject) => {
+        this.result = localStorage.setItem(key, value);
+        resolve(this.result)
+    })
+  }
+
+  get(key){
+    return new Promise((resolve, reject) => {
+      this.result = localStorage.getItem(key)
+      resolve(this.result)
+    })
+  }
+
+  delete(key){
+    return new Promise((resolve, reject) => {
+      this.result = localStorage.removeItem(key)
+      resolve(this.result)
+    })
+  }
+  
+} window.customElements.define('blox-store', BloxStore);
